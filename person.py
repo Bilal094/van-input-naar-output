f3f950500ce21e48528a2ee5d9c29fd2bc694760
Bilal094 <99066318@mydavinci.nl>
2021-09-08 11:08:47 +0200

    Programma gemaakt

---
+++ person.py
@@ -0,0 +1,19 @@
+print('Naam invoeren')
+name = input('Wat is je naam')
+print('Welkom' + name)
+
+print('Adresgegevens')
+adress = input('Wat is je adres')
+
+print('Postcode')
+postalcode = input('Wat is je postcode')
+
+print('Woonplaats')
+residence = input('Waar woon je')
+
+print('----------------------------------------------------')
+print('|  Naam      :'   + name)
+print('|  Adres     :'   + adress)
+print('|  Postcode  :'   + postalcode)
+print('|  Woonplaats:'   + residence)
+print('----------------------------------------------------')
